0800390834bbdf1e0e70db977471c48c0f5ecdf8
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-17 02:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
   
   for (let i=0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart[i].splice(i,1);
+      cart.splice(i,1);
       return cart;
     } 
   } 
