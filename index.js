7f756a5f5d57cb3938d42b574286bce1748b66a1
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-17 03:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,5 +71,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return 'Sorry, we don\'t have a credit card on file for you.';
+  } else {
+    var order = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return order;
   }
 }
